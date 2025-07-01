486d81fef464a20051c15ba9e21f95de1a3d95e3
amatorciast <amatorciast@gmail.com>
2025-07-01 18:46:46 +0200

    some placeholder code

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,19 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-    res.send("you got 5 notes")
+    res.send("you got 20 notes")
 });
+
+app.post("api/notes", (req, res) => {
+    res.status(201).json({message:"post created succesfully!"})
+})
+
+app.put("api/notes/:id", (req, res) => {
+    res.status(201).json({message:"post updated succesfully!"})
+})
+
+app.delete("api/notes/:id", (req, res) => {
+    res.status(201).json({message:"post deleted succesfully!"})
+})
 
 app.listen(5001, () => {console.log("Server started on PORT: 5001")});
