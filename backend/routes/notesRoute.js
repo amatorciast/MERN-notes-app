2ad05fd7719976ed4984e0b31d460d341d7d20e1
amatorciast <amatorciast@gmail.com>
2025-07-03 15:40:38 +0200

    work please

--- backend/routes/notesRoute.js
+++ backend/routes/notesRoute.js
@@ -2,4 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
+
+router.get("", (req, res) => {
+    res.status(200).send("you got 20 notes")
+});
+
+router.post("", (req, res) => {
+    res.status(201).json({message:"post created succesfully!"})
+})
+
+router.put("/:id", (req, res) => {
+    res.status(200).json({message:"post updated succesfully!"})
+})
+
+router.delete("/:id", (req, res) => {
+    res.status(201).json({message:"post deleted succesfully!"})
+})
+
 export default router;
