d42e6f03110de3a6dc7ea4f7c6c16ec9e3f9c49c
amatorciast <amatorciast@gmail.com>
2025-07-03 16:41:28 +0200

    some security updates, .env I mean

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://amatorciast:abcdefghijk@cluster0.qzhq7ml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected successfully!")
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoute.js";
 import { connectDB } from "./config/db.js";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
 
