c66b0445afba4ba7dac045c973eadc3760949807
amatorciast <amatorciast@gmail.com>
2025-07-07 17:34:43 +0200

    thats all for today, maybe

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,7 +1,7 @@
 import Note from "../models/Note.js";
 export async function getAllNotes (req, res) {
     try {
-        const notes = await Note.find()
+        const notes = await Note.find().sort({CreatedAt: -1})
         res.status(200).json(notes);
     } catch (error) {
         console.error("Error in getAllNotes controller", error);
